f0843600eb6697021235bb0308be02fd37dbfa3d
Anderson Ocaña <andru.ocatorres@gmail.com>
2024-11-14 21:14:23 -0300

    procedures last

--- 15_funciones/clase.sql
+++ 15_funciones/clase.sql
@@ -1,231 +1,289 @@
-USE funciones;
+-- ETL
+DROP DATABASE IF EXISTS procedures;
+CREATE DATABASE procedures;
 
--- me devuelva la cantidad de ciudadanos que tengo en un pais
--- siempre y cuando exista ese pais
+USE procedures;
 
--- VARIABLES -> 
+-- SELECT * FROM coderhouse_gamers.SYSTEM_USER;
+-- ME TRANSFORME DATOS DE USUARIOS DE coderhouse_gamers y que le anexe 
+-- bajo ciertos criterios de negocio una ciudad.
 
-SET @pais = "%almania%";
+-- EXTRACT --> 2 bases de datos
+-- 	usuarios + ciudades que le agregar
+--     
+-- TRANSFORM -> estos usuarios agregarle data y adaptarlo a la necesidad del negocio
 
+-- LOAD --> tabla dentro de una base de datos que maneje cierto negocio
 
-SELECT 
-    COUNT(ci.id_ciudadano) AS total_ciudadanos
-FROM funciones.pais AS p
-LEFT JOIN funciones.ciudad AS c 
-	ON p.id_pais = c.id_pais
-LEFT JOIN funciones.ciudadano AS ci
-	ON c.id_ciudad = ci.id_ciudad
-WHERE p.nombre LIKE @pais 
-GROUP BY p.id_pais
-;
-
-
-DROP FUNCTION IF EXISTS funciones.fx_total_ciudadanos;
+-- que tengan .edu .pl .gov --> munich
+-- .com --> paris
+-- .org que el resto
 
 DELIMITER //
-CREATE FUNCTION funciones.fx_total_ciudadanos(_pais VARCHAR(200))
-	RETURNS INT
+DROP FUNCTION IF EXISTS procedures.fx_search_city //
+
+CREATE FUNCTION 
+	procedures.fx_search_city( _mail VARCHAR(200)) 
+RETURNS VARCHAR(200)
     READS SQL DATA
 BEGIN
 	
-    DECLARE valor_retorno INT DEFAULT 0;
+    DECLARE _id_ciudad INT ;
+	DECLARE nombre_ciudad VARCHAR(200) DEFAULT 'ciudad no reconocida';
     
-	SELECT 
-		COUNT(ci.id_ciudadano) INTO valor_retorno
-	FROM funciones.pais AS p
-	LEFT JOIN funciones.ciudad AS c 
-		ON p.id_pais = c.id_pais
-	LEFT JOIN funciones.ciudadano AS ci
-		ON c.id_ciudad = ci.id_ciudad
-	WHERE p.nombre LIKE _pais
-	GROUP BY p.id_pais;
+    CASE 
+		WHEN _mail LIKE '%.com' THEN 
+			SET _id_ciudad = 3;
+		WHEN _mail REGEXP '\\.(edu|pl|gov)$' THEN 
+			SET _id_ciudad = 8;
+        ELSE
+			SET _id_ciudad = 10 ;
+	END CASE ;
+	
+    SELECT nombre INTO nombre_ciudad
+		FROM funciones.ciudad
+	WHERE id_ciudad = _id_ciudad;
+	
+	RETURN nombre_ciudad;
 
-	-- If no citizens are found, raise an error
-    IF valor_retorno = 0 THEN
-        SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'No citizens found for the specified country', MYSQL_ERRNO = 1000;
-    END IF;
-
-
-	RETURN valor_retorno;
-
-END  //
+END //
 DELIMITER ;
 
 
-
-
-SELECT 
-	p.*,
-	fx_total_ciudadanos(p.nombre)
-FROM funciones.pais AS p;
-
-
-
-
-SELECT 
-	'Peru' AS pais,
-	fx_total_ciudadanos('Peru') AS total_ciudadanos
-FROM DUAL
-
-;
-
-SHOW WARNINGS;
-
-
-INSERT INTO funciones.ciudadano_doc (id_doc, num_doc, id_ciudadano) VALUES
-(1, '28456789', 1),
-(2, '29567834', 2),
-(3, '30789123', 3),
-(4, '31234567', 4),
-(5, '32456789', 5),
-(6, '33567890', 6),
-(7, '34678912', 7),
-(8, '35789023', 8),
-(9, '36890134', 9),
-(10, '37901245', 10),
-(11, '38012356', 11),
-(12, '39123467', 12),
-(13, '40234578', 13),
-(14, '41345689', 14),
-(15, '42456790', 15),
-(16, '43567801', 16),
-(17, '44678912', 17),
-(18, '45789023', 18),
-(19, '46890134', 19),
-(20, '47901245', 20),
-(21, '28901256', 21),
-(22, '29012367', 22),
-(23, '30123478', 23),
-(24, '31234589', 24),
-(25, '32345690', 25),
-(26, '33456701', 26),
-(27, '34567812', 27),
-(28, '35678923', 28),
-(29, '36789034', 29),
-(30, '37890145', 30),
-(31, '38901256', 31),
-(32, '39012367', 32),
-(33, '40123478', 33),
-(34, '41234589', 34),
-(35, '42345690', 35),
-(36, '43456701', 36),
-(37, '44567812', 37),
-(38, '45678923', 38),
-(39, '46789034', 39),
-(40, '47890145', 40),
-(41, '28901267', 41),
-(42, '29012378', 42),
-(43, '30123489', 43),
-(44, '31234590', 44),
-(45, '32345601', 45);
-
-
--- una funcion que dependiendo del pais
--- del documento del cuidadano de mayor edad
-
-DROP FUNCTION IF EXISTS funciones.fx_calculo_edad;
-
 DELIMITER //
-CREATE FUNCTION funciones.fx_calculo_edad(_doc VARCHAR(8))
-RETURNS INT
-DETERMINISTIC
+DROP PROCEDURE IF EXISTS procedures.sp_transform_load_users //
+CREATE PROCEDURE procedures.sp_transform_load_users( 
+	IN first_char VARCHAR(2)
+)
 BEGIN
 	
-	DECLARE primeros_dos_digitos INT;
-    DECLARE anio_aproximado INT;
-    DECLARE edad_aproximada INT;
-
-    -- Extraer los primeros dos dígitos del DNI
-    SET primeros_dos_digitos = CAST(LEFT(_doc, 2) AS UNSIGNED);
-    
-    -- Determinar el año aproximado de nacimiento
-    -- DNIs que empiezan con 4-5: nacidos después de 2000
-    -- DNIs que empiezan con 2-3: nacidos entre 1970-1999
-    CASE
-		WHEN primeros_dos_digitos >= 45 THEN
-			SET anio_aproximado = 2000 + primeros_dos_digitos - 45;
-		WHEN primeros_dos_digitos >= 40 THEN
-			SET anio_aproximado = 2000 + primeros_dos_digitos - 40;
-		WHEN primeros_dos_digitos >= 30 THEN
-			SET anio_aproximado = 1900 + primeros_dos_digitos;
-		WHEN primeros_dos_digitos >= 20 THEN
-			SET anio_aproximado = 1900 + primeros_dos_digitos;
-    ELSE
-        SET anio_aproximado = 2000 + primeros_dos_digitos;
-        
-    END CASE;
-    
-    -- Calcular la edad aproximada
-    SET edad_aproximada = YEAR(CURRENT_DATE()) - anio_aproximado;
-    
-    RETURN edad_aproximada;
+	
+    INSERT INTO `procedures`.`usuarios_nuevos_productiva` 
+    (first_name,last_name,email,ciudad)
+    SELECT
+		s.first_name,
+		s.last_name,
+		s.email,
+		procedures.fx_search_city(s.email) AS ciudad
+	FROM
+	coderhouse_gamers.SYSTEM_USER AS s
+    WHERE 
+		s.first_name LIKE CONCAT( first_char ,'%');
 END //
 
 DELIMITER ;
 
+CREATE TABLE `procedures`.`usuarios_nuevos_productiva` (
+  `usuario_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `ciudad` varchar(200) DEFAULT NULL,
+  stamp_upload DATETIME DEFAULT (CURRENT_TIMESTAMP)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CALL procedures.sp_transform_load_users('z');
 
 
-SELECT 
-	fx_calculo_edad('32345601')
-FROM DUAL;
 
+-- 
+-- creacion de un usuario
+-- =>  validar si el usuario existe, si no existe crearlo
+-- =>  validar que la ciudad exista dentro de la base de datos, si no crearlo
 
+-- procedure => 
+-- 	validacion de existencia
 
-DROP FUNCTION IF EXISTS funciones.fx_get_senior_citizen;
+-- procedure
+-- 	creacion de ciudad
+--     creacion de pais
+
+-- procedure
+-- 	ingreso de datos
 
 DELIMITER //
-CREATE FUNCTION funciones.fx_get_senior_citizen(_pais VARCHAR(100))
-RETURNS VARCHAR(100)
-DETERMINISTIC
-COMMENT 'Esta funcion es para quemarles el bocho a los alumnos'
+DROP PROCEDURE IF EXISTS procedures.sp_validacion //
+CREATE PROCEDURE procedures.sp_validacion(
+ IN _name VARCHAR(100) , IN _last_name VARCHAR(200), IN _email VARCHAR(200)
+,IN _pais VARCHAR(30), IN _ciudad VARCHAR(30)
+,OUT existencia_usuario INT
+,OUT existencia_pais INT
+,OUT existencia_ciudad INT
+)BEGIN
+	
+    SET existencia_usuario = 0;
+    SET existencia_pais = 0;
+    SET existencia_ciudad = 0;
+
+	-- validar existencia de usuario
+	SELECT 
+		id_system_user INTO existencia_usuario
+    FROM coderhouse_gamers.SYSTEM_USER
+    WHERE email = _email
+    AND last_name = _last_name
+    AND first_name = _name
+    LIMIT 1
+    ;
+
+	
+	SELECT 
+		c.id_ciudad , p.id_pais INTO existencia_ciudad, existencia_pais
+    FROM funciones.ciudad AS c
+    INNER JOIN funciones.pais AS p
+		USING(id_pais)
+	WHERE c.nombre = _ciudad AND p.nombre = _pais;
+    
+END //
+
+
+DROP PROCEDURE IF EXISTS procedures.sp_ciudadano //
+CREATE PROCEDURE procedures.sp_ciudadano(
+ IN _name VARCHAR(100) , IN _last_name VARCHAR(200), IN _email VARCHAR(200)
+ ,OUT existencia_usuario INT)
+ BEGIN
+	DECLARE id INT ;
+ 
+ 	IF existencia_usuario =  0 THEN
+		SELECT id_system_user INTO id
+        FROM coderhouse_gamers.SYSTEM_USER
+		ORDER BY id_system_user DESC
+        LIMIT 1;
+        
+        SET id = id + 1;
+    
+		INSERT INTO coderhouse_gamers.SYSTEM_USER
+        VALUE(id, _name,_last_name,_email, "change_me!", 1);
+        
+        SET existencia_usuario = id;
+    END IF;
+    
+ END //
+ 
+ 
+DROP PROCEDURE IF EXISTS procedures.sp_ciudad_pais //
+CREATE PROCEDURE procedures.sp_ciudad_pais(
+ IN _pais VARCHAR(30), IN _ciudad VARCHAR(30)
+,OUT existencia_pais INT
+,OUT existencia_ciudad INT
+)
 BEGIN
 
-DECLARE nombre_retorno VARCHAR(100) DEFAULT 'No existe';
-DECLARE existe_pais BOOL DEFAULT TRUE; 
+	IF existencia_pais =  0 THEN
+		INSERT INTO funciones.pais
+        (nombre)
+        VALUE(_pais);
+        SELECT LAST_INSERT_ID() INTO existencia_pais;
+    END IF;
+    
+    IF existencia_pais =  0 THEN
+		
+		INSERT INTO funciones.ciudad
+        (nombre,id_pais)
+        VALUE(_ciudad,existencia_ciudad);
+        
+        SELECT LAST_INSERT_ID() INTO existencia_ciudad;
+    END  IF;
 
-SELECT COUNT(*) > 0 INTO existe_pais
-FROM funciones.pais AS p
-WHERE p.nombre LIKE _pais;
+END;
 
 
-IF existe_pais != TRUE THEN
-	SET nombre_retorno = "No existe pais";
-    RETURN nombre_retorno ;
-END IF;
-
-
-SELECT 
-	nombre INTO nombre_retorno
-FROM (
-
-SELECT 
-		p.id_pais,
-		ci.nombre,
-		fx_calculo_edad(cd.num_doc) AS edad
-	FROM funciones.pais AS p
-	LEFT JOIN funciones.ciudad AS c 
-		ON p.id_pais = c.id_pais
-	LEFT JOIN funciones.ciudadano AS ci
-		ON c.id_ciudad = ci.id_ciudad
-	LEFT JOIN funciones.ciudadano_doc AS cd
-		ON ci.id_ciudadano = cd.id_ciudadano
-	WHERE p.nombre LIKE _pais) AS tabla_edades
-		ORDER BY edad DESC LIMIT 1;
-
-RETURN nombre_retorno;
-
+DROP PROCEDURE IF EXISTS procedures.load_user //
+CREATE PROCEDURE procedures.load_user( 
+ OUT existencia_usuario INT
+,OUT existencia_ciudad INT
+)
+BEGIN
+	
+	
+    INSERT INTO `procedures`.`usuarios_nuevos_productiva` 
+    (first_name,last_name,email,ciudad)
+    SELECT
+		s.first_name,
+		s.last_name,
+		s.email,
+		c.nombre
+	FROM coderhouse_gamers.SYSTEM_USER AS s,
+		funciones.ciudad AS c
+    
+    WHERE 
+		id_system_user = existencia_usuario
+        AND c.id_ciudad = existencia_ciudad;
 END //
+DELIMITER ;
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS procedures.main_proc //
+
+CREATE PROCEDURE procedures.main_proc(
+ IN _name VARCHAR(100) , IN _last_name VARCHAR(200), IN _email VARCHAR(200)
+,IN _pais VARCHAR(30), IN _ciudad VARCHAR(30)
+,INOUT existencia_usuario INT
+,INOUT existencia_pais INT
+,INOUT existencia_ciudad INT
+)
+BEGIN 
+	CALL procedures.sp_validacion(
+    _name,
+    _last_name,
+    _email,
+    _pais,
+    _ciudad,
+	@existencia_usuario,
+	@existencia_ciudad ,
+	@existencia_pais) ;
+    
+    CALL procedures.sp_ciudad_pais(
+		_pais ,
+        _ciudad ,
+		@existencia_pais,
+		@existencia_ciudad
+	);
+    
+    
+    CALL procedures.sp_ciudadano(
+		_name,
+        _last_name,
+        _email,
+        @existencia_usuario ) ;
+        
+	
+    CALL procedures.load_user( 
+		@existencia_usuario,
+		@existencia_ciudad
+	);
+    
+END //
+
 
 DELIMITER ;
 
 
+SET @existencia_usuario = 0;
+SET @existencia_ciudad = 0;
+SET @existencia_pais = 0;
+
+CALL procedures.main_proc(
+'jorge',
+'amancio',
+'mail@mail.com',
+'Francia' ,
+'París',
+@existencia_usuario,
+@existencia_ciudad ,
+@existencia_pais
+);
 
 
 SELECT 
-	p.*,
-	fx_get_senior_citizen(p.nombre) AS ciudadano_mayor
-FROM funciones.pais AS p;
+@existencia_usuario,
+@existencia_ciudad ,
+@existencia_pais FROM DUAL;
+
+
+
+
+
+
 
 
 
