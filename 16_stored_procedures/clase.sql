8f547881950debbceeb2350ce62fc2cdaeac8f51
Anderson Ocaña <andru.ocatorres@gmail.com>
2024-11-15 19:01:40 -0300

    clase. -> after

--- 16_stored_procedures/clase.sql
+++ 16_stored_procedures/clase.sql
@@ -1,95 +1,3 @@
--- ETL
-DROP DATABASE IF EXISTS procedures;
-CREATE DATABASE procedures;
-
-USE procedures;
-
--- SELECT * FROM coderhouse_gamers.SYSTEM_USER;
--- ME TRANSFORME DATOS DE USUARIOS DE coderhouse_gamers y que le anexe 
--- bajo ciertos criterios de negocio una ciudad.
-
--- EXTRACT --> 2 bases de datos
--- 	usuarios + ciudades que le agregar
---     
--- TRANSFORM -> estos usuarios agregarle data y adaptarlo a la necesidad del negocio
-
--- LOAD --> tabla dentro de una base de datos que maneje cierto negocio
-
--- que tengan .edu .pl .gov --> munich
--- .com --> paris
--- .org que el resto
-
-DELIMITER //
-DROP FUNCTION IF EXISTS procedures.fx_search_city //
-
-CREATE FUNCTION 
-	procedures.fx_search_city( _mail VARCHAR(200)) 
-RETURNS VARCHAR(200)
-    READS SQL DATA
-BEGIN
-	
-    DECLARE _id_ciudad INT ;
-	DECLARE nombre_ciudad VARCHAR(200) DEFAULT 'ciudad no reconocida';
-    
-    CASE 
-		WHEN _mail LIKE '%.com' THEN 
-			SET _id_ciudad = 3;
-		WHEN _mail REGEXP '\\.(edu|pl|gov)$' THEN 
-			SET _id_ciudad = 8;
-        ELSE
-			SET _id_ciudad = 10 ;
-	END CASE ;
-	
-    SELECT nombre INTO nombre_ciudad
-		FROM funciones.ciudad
-	WHERE id_ciudad = _id_ciudad;
-	
-	RETURN nombre_ciudad;
-
-END //
-DELIMITER ;
-
-
-DELIMITER //
-DROP PROCEDURE IF EXISTS procedures.sp_transform_load_users //
-CREATE PROCEDURE procedures.sp_transform_load_users( 
-	IN first_char VARCHAR(2)
-)
-BEGIN
-	
-	
-    INSERT INTO `procedures`.`usuarios_nuevos_productiva` 
-    (first_name,last_name,email,ciudad)
-    SELECT
-		s.first_name,
-		s.last_name,
-		s.email,
-		procedures.fx_search_city(s.email) AS ciudad
-	FROM
-	coderhouse_gamers.SYSTEM_USER AS s
-    WHERE 
-		s.first_name LIKE CONCAT( first_char ,'%');
-END //
-
-DELIMITER ;
-
-CREATE TABLE `procedures`.`usuarios_nuevos_productiva` (
-  `usuario_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `ciudad` varchar(200) DEFAULT NULL,
-  stamp_upload DATETIME DEFAULT (CURRENT_TIMESTAMP)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CALL procedures.sp_transform_load_users('z');
-
--- User Creation Procedures
--- Steps:
--- 1. Validate if user exists, if not create
--- 2. Validate if city exists in database, if not create
--- 3. Handle data insertion
-
 DELIMITER //
 
 -- Procedure 1: Validation of existence
@@ -152,27 +60,49 @@ BEGIN
 END //
 
 -- Procedure 3: City and Country Creation
-DROP PROCEDURE IF EXISTS procedures.sp_ciudad_pais //
-CREATE PROCEDURE procedures.sp_ciudad_pais(
+DROP PROCEDURE IF EXISTS procedures.sp_pais //
+CREATE PROCEDURE procedures.sp_pais(
     IN _pais VARCHAR(30),
-    IN _ciudad VARCHAR(30),
-    INOUT existencia_pais INT,
-    INOUT existencia_ciudad INT
+    INOUT existencia_pais INT
 )
 BEGIN
     IF existencia_pais = 0 THEN
+
+        SELECT _pais  AS pais;
+
         INSERT INTO funciones.pais (nombre)
         VALUE(_pais);
+
         SELECT LAST_INSERT_ID() INTO existencia_pais;
     END IF;
     
-    IF existencia_pais = 0 THEN
+
+END //
+
+-- Procedure 3: City and Country Creation
+DROP PROCEDURE IF EXISTS procedures.sp_ciudad //
+CREATE PROCEDURE procedures.sp_ciudad(
+    IN _ciudad VARCHAR(30),
+    INOUT existencia_ciudad INT,
+    INOUT existencia_pais INT
+)
+BEGIN
+
+    IF existencia_ciudad = 0 THEN
+
+        SELECT _ciudad AS ciudad;
+
         INSERT INTO funciones.ciudad (nombre, id_pais)
-        VALUE(_ciudad, existencia_ciudad);
+        VALUE(_ciudad, existencia_pais);
         
         SELECT LAST_INSERT_ID() INTO existencia_ciudad;
     END IF;
+
 END //
+
+
+
+
 
 -- Procedure 4: User Data Loading
 DROP PROCEDURE IF EXISTS procedures.load_user //
@@ -219,20 +149,27 @@ BEGIN
     );
     
     SELECT 
-        @existencia_usuario != 0 AS existencia_usuario,
-        @existencia_ciudad != 0 AS existencia_ciudad,
-        @existencia_pais != 0 AS existencia_pais;
+        @existencia_usuario AS existencia_usuario,
+        @existencia_ciudad AS existencia_ciudad,
+        @existencia_pais  AS existencia_pais;
 
-    CALL procedures.sp_ciudad_pais(
+    CALL procedures.sp_pais(
         _pais,
-        _ciudad,
-        @existencia_pais,
-        @existencia_ciudad
+        @existencia_pais
+  
     );
 
+
+    CALL procedures.sp_ciudad(
+        _ciudad,
+        @existencia_ciudad,
+        @existencia_pais
+    );
+
+
     SELECT 
-        @existencia_ciudad != 0 AS existencia_ciudad,
-        @existencia_pais != 0 AS existencia_pais;
+        @existencia_ciudad AS existencia_ciudad,
+        @existencia_pais AS existencia_pais;
 
     CALL procedures.sp_ciudadano(
         _name,
@@ -241,7 +178,7 @@ BEGIN
         @existencia_usuario
     );
     
-    SELECT @existencia_usuario != 0 AS existencia_usuario;
+    SELECT @existencia_usuario AS existencia_usuario;
     
     CALL procedures.load_user(
         @existencia_usuario,
